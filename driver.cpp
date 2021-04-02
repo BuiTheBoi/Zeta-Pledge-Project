feb4cd535289ced7c4ccd1e6c18ff4163d7f2491
BuiTheBoi <uNUKEcorn15@gmail.com>
2021-04-02 09:56:08 -0700

    New class implemented :rocket:

---
+++ driver.cpp
@@ -0,0 +1,23 @@
+#include "plant.hpp"
+
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<Plant> mySaves[3] = {
+        Plant("DEFAULT", );
+        }
+
+    // View the screen (Start and options)
+    
+
+    // If options, take user input
+
+    // If start, choose which save (plant) to use to water plants
+
+
+    Plant myPlant("Cactus", 10, 10, 10, "PM");
+
+    
+    return 0;
+}
---
+++ plant.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Plant {
+private:
+    string plantName_,      // The name of your plant
+            AMOrPM_;        // Time being AM or PM
+    int waterFlow_,         // How much water to give at certain time (Capacity will be determined)
+        hour_,              // 1 - 12
+        minute_;            // 0 - 59
+
+public:
+    // Constructors
+    Plant(string, int, int, int, string);
+    Plant();    // Default
+
+    // Getters
+    string getPlantName();
+    string getAMOrPM();
+    int getWaterFlow();
+    int getHour();
+    int getMinute();
+
+    // Setters
+    void setPlantName(string);
+    void setAMOrPM(string);
+    void setWaterFlow(int);
+    void setHour(int);
+    void setMinute(int);
+};
+
+
+// ========================================== FUNCTION BODIES ==========================================
+
+// --------------- CONSTRUCTORS ---------------
+Plant::Plant(string plantName, int waterFlow, int hour, int minute, string AM_PM): 
+        plantName_(plantName), waterFlow_(waterFlow), hour_(hour), minute_(minute), AMOrPM_(AM_PM) {}; 
+
+Plant::Plant(): // Default value: UNIDENTIFIED name pouring 30 units of water at 12:00 PM
+    plantName_("UNIDENTIFIED"), waterFlow_(30), hour_(12), minute_(0), AMOrPM_("PM") {};
+
+
+// --------------- GETTERS ---------------
+string Plant::getPlantName() { return plantName_; }
+
+string Plant::getAMOrPM() {return AMOrPM_; }
+
+int Plant::getWaterFlow() { return waterFlow_; }
+
+int Plant::getHour() { return hour_; }
+
+int Plant::getMinute() { return minute_; }
+
+
+// --------------- SETTERS ---------------
+
+void Plant::setPlantName(string plantName) { plantName_ = plantName; }
+
+void Plant::setAMOrPM(string AMOrPM) { AMOrPM_ = AMOrPM; }
+
+void Plant::setWaterFlow(int waterFlow) { waterFlow_ = waterFlow; }
+
+void Plant::setHour(int hour) { hour_ = hour; }
+
+void Plant::setMinute(int minute) { minute_ = minute; }
